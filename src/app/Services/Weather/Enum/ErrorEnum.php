2c2a4f2cc10f2c6cc07a19a5c8ce021717eedba9
Mikhail Ilinsky <mail@milinsky.ru>
2021-11-05 20:06:16 +1000

    Fix enum variables

--- src/app/Services/Weather/Enum/ErrorEnum.php
+++ src/app/Services/Weather/Enum/ErrorEnum.php
@@ -9,6 +9,6 @@ class ErrorEnum
     public const INTERNAL_ERROR = 'Internal error';
     public const GEO_POSITION_ERROR = 'Location is not defined';
     public const INVALID_PERIOD_PARAM_ERROR = 'Invalid period parameter given';
-    public const VALUE_FOR_RERIOD_PASSED_ERROR = "The value for the 'period' key was not passed";
+    public const VALUE_FOR_PERIOD_PASSED_ERROR = "The value for the 'period' key was not passed";
     public const VALUE_FOR_CITY_PASSED_ERROR = "The value for the 'city' key was not passed";
 }
