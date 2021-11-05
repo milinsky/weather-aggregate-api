1b02e3286e6fc1290eb081c22d1ceeb41645164f
Mikhail Ilinsky <mail@milinsky.ru>
2021-11-05 19:49:59 +1000

    Fix validation

--- src/app/Services/Weather/Enum/ErrorEnum.php
+++ src/app/Services/Weather/Enum/ErrorEnum.php
@@ -6,6 +6,7 @@ namespace App\Services\Weather\Enum;
 
 class ErrorEnum
 {
+    public const INTERNAL_ERROR = 'Internal error';
     public const GEO_POSITION_ERROR = 'Location is not defined';
     public const INVALID_PERIOD_PARAM_ERROR = 'Invalid period parameter given';
     public const VALUE_FOR_RERIOD_PASSED_ERROR = "The value for the 'period' key was not passed";
